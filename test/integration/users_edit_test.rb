bbc32796d68c5b8536553c9204c7125fc614f48f
Adefemi Adeyemi <ImperiousEnterprise@hotmail.com>
2015-02-24 16:57:29 +0900

    Chapter 9 exercises added

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -38,16 +38,42 @@ class UsersEditTest < ActionDispatch::IntegrationTest
       get edit_user_path(@user)
       log_in_as(@user)
       assert_redirected_to edit_user_path(@user)
-      name  = "Foo Bar"
-      email = "foo@bar.com"
-      patch user_path(@user), user: { name:  name,
-                                      email: email,
-                                      password:              "foobar",
-                                      password_confirmation: "foobar" }
-      assert_not flash.empty?
+      log_in_as(@user)
       assert_redirected_to @user
-      @user.reload
-      assert_equal @user.name,  name
-      assert_equal @user.email, email
+#      name  = "Foo Bar"
+#      email = "foo@bar.com"
+#      patch user_path(@user), user: { name:  name,
+#                                      email: email,
+#                                      password:              "foobar",
+#                                      password_confirmation: "foobar" }
+#      
+#      assert_not flash.empty?
+#      assert_redirected_to @user
+#      @user.reload
+#      assert_equal @user.name,  name
+#      assert_equal @user.email, email
+    
     end
+    
+   test "layout links" do
+     get root_path
+     # Not logged in so a "Login" link will be present
+     assert_select "a[href=?]", login_path
+     assert_select "a[href=?]", root_path
+     assert_select "a[href=?]", help_path
+     assert_select "a[href=?]", signup_path
+     # Login
+     log_in_as(@user)
+     # Go to root
+     get root_path
+     # Now logged in so a "Login" link won't be present
+     assert_select "a[href=?]", login_path, count: 0
+     # Now logged in so a "Logout" link will be present
+     assert_select "a[href=?]", logout_path
+     # Now logged in so a "Users" link will be present
+     assert_select "a[href=?]", users_path
+     # Now logged in so a "Accounts" link will be present
+     assert_select 'a[href=?]', '#', text: 'Account'
+     
+   end
 end
