cd414ea4336904e498499253b397ea8cf12c06e0
Adefemi Adeyemi <ImperiousEnterprise@hotmail.com>
2015-03-05 11:36:27 +0900

    Added test for picture uploads and counting sidebar

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -8,31 +8,47 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
      @user = users(:adefemi)
    end
  
-   test "micropost interface" do
+  
+  test "micropost sidebar count" do
      log_in_as(@user)
      get root_path
-     assert_select 'div.pagination'
-     # Invalid submission
-     assert_no_difference 'Micropost.count' do
-       post microposts_path, micropost: { content: "" }
-     end
-     assert_select 'div#error_explanation'
-     # Valid submission
-     content = "This micropost really ties the room together"
-     assert_difference 'Micropost.count', 1 do
-       post microposts_path, micropost: { content: content }
-     end
-     assert_redirected_to root_url
-     follow_redirect!
-     assert_match content, response.body
-     # Delete a post.
-     assert_select 'a', text: 'delete'
-     first_micropost = @user.microposts.paginate(page: 1).first
-     assert_difference 'Micropost.count', -1 do
-       delete micropost_path(first_micropost)
-     end
-     # Visit a different user.
-     get user_path(users(:archer))
-     assert_select 'a', text: 'delete', count: 0
-   end
+     assert_match "#{@user.microposts.count} microposts", response.body
+     # User with zero microposts
+     other_user = users(:mallory)
+      log_in_as(other_user)
+      get root_path
+      assert_match "0 microposts", response.body
+      other_user.microposts.create!(content: "A micropost")
+      get root_path
+      assert_match "1 micropost", response.body
+    end
+    
+  test "micropost interface" do
+      log_in_as(@user)
+      get root_path
+      assert_select 'div.pagination'
+      assert_select 'input[type=file]'
+      # Invalid submission
+      post microposts_path, micropost: { content: "" }
+      assert_select 'div#error_explanation'
+      # Valid submission
+      content = "This micropost really ties the room together"
+      picture = fixture_file_upload('test/fixtures/imagetest.jpg', 'image/jpeg')
+      assert_difference 'Micropost.count', 1 do
+        post microposts_path, micropost: { content: content, picture: picture }
+      end
+      pic = assigns(:micropost)
+      assert pic.picture?
+      follow_redirect!
+      assert_match content, response.body
+      # Delete a post.
+      assert_select 'a', 'delete'
+      first_micropost = @user.microposts.paginate(page: 1).first
+      assert_difference 'Micropost.count', -1 do
+        delete micropost_path(first_micropost)
+      end
+      # Visit a different user.
+      get user_path(users(:archer))
+      assert_select 'a', { text: 'delete', count: 0 }
+    end
 end
